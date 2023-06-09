7a11cb504c800e9735d69c0b5b6af6698e5822f2
Pratit Todkar <pratit@connectia.in>
2023-06-10 04:50:07 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,39 @@
-import 'dart:developer';
-
-import 'package:agora_rtc_engine/agora_rtc_engine.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
 
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
-void main() => runApp(const MaterialApp(home: JoinChannelAudio()));
+const String appId = "d776258684954df1a6813c77c705968f";
 
-/// JoinChannelAudio Example
-class JoinChannelAudio extends StatefulWidget {
-  /// Construct the [JoinChannelAudio]
-  const JoinChannelAudio({Key? key}) : super(key: key);
+void main() => runApp(const MaterialApp(home: MyApp()));
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => _State();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _State extends State<JoinChannelAudio> {
-  late final RtcEngine _engine;
-  String token = "";
-  int uid = 0;
-  String channelName = "GamersGuardians";
-  bool isJoined = false, openMicrophone = true, enableSpeakerphone = true, playEffect = false;
-  bool _enableInEarMonitoring = false;
-  double _recordingVolume = 100, _playbackVolume = 100, _inEarMonitoringVolume = 100;
-  late TextEditingController _controller;
-  ChannelProfileType _channelProfileType = ChannelProfileType.channelProfileLiveBroadcasting;
+class _MyAppState extends State<MyApp> {
   int tokenRole = 1; // use 1 for Host/Broadcaster, 2 for Subscriber/Audience
   String serverUrl = "https://agora-token-service-production-66b2.up.railway.app"; // The base URL to your token server, for example "https://agora-token-service-production-92ff.up.railway.app"
   int tokenExpireTime = 45; // Expire time in Seconds.
   bool isTokenExpiring = false; // Set to true when the token is about to expire
-  final channelTextController = TextEditingController(text: ''); // To access the TextField
+  final channelTextController = TextEditingController(text: 'GamersGuardians'); // To access the TextField
+  String channelName = "GamersGuardians";
+  String token = "";
+
+  int uid = 0; // uid of the local user
+
+  int? _remoteUid; // uid of the remote user
+  bool _isJoined = false; // Indicates if the local user has joined the channel
+  late RtcEngine agoraEngine; // Agora engine instance
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey
+  = GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
 
   Future<void> fetchToken(int uid, String channelName, int tokenRole) async {
     // Prepare the Url
@@ -62,297 +62,179 @@ class _State extends State<JoinChannelAudio> {
 
     if (isTokenExpiring) {
       // Renew the token
-    _engine.renewToken(token);
+      agoraEngine.renewToken(token);
       isTokenExpiring = false;
-      log("Token renewed");
+      showMessage("Token renewed");
     } else {
       // Join a channel.
-      log("Token received, joining a channel...");
+      showMessage("Token received, joining a channel...");
+      // Set channel options including the client role and channel profile
+      ChannelMediaOptions options = const ChannelMediaOptions(
+        clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        channelProfile: ChannelProfileType.channelProfileCommunication,
+      );
 
-      await _engine.joinChannel(
+      await agoraEngine.joinChannel(
         token: token,
         channelId: channelName,
-        uid: uid, options: ChannelMediaOptions(
-        channelProfile: _channelProfileType,
-        clientRoleType: ClientRoleType.clientRoleBroadcaster,
-      ),
+        options: options,
+        uid: uid,
       );
     }
   }
 
 
-  @override
-  void initState() {
-    super.initState();
-    _controller = TextEditingController(text: channelName);
-    _initEngine();
-  }
+  Future<void> setupVoiceSDKEngine() async {
+    // retrieve or request microphone permission
+    await [Permission.microphone].request();
 
-  @override
-  void dispose() {
-    super.dispose();
-    _dispose();
-  }
-
-  Future<void> _dispose() async {
-    await _engine.leaveChannel();
-    await _engine.release();
-  }
-
-  Future<void> _initEngine() async {
-    _engine = createAgoraRtcEngine();
-    await _engine.initialize(const RtcEngineContext(
-      appId: "d776258684954df1a6813c77c705968f",
+    //create an instance of the Agora engine
+    agoraEngine = createAgoraRtcEngine();
+    await agoraEngine.initialize(const RtcEngineContext(
+        appId: appId
     ));
 
-    _engine.registerEventHandler(RtcEngineEventHandler(
-      onTokenPrivilegeWillExpire: (RtcConnection connection, String token) {
-        log('Token expiring');
-        isTokenExpiring = true;
-        setState(() {
-          // fetch a new token when the current token is about to expire
-          fetchToken(uid, channelName, tokenRole);
-        });
-      },
-
-      onError: (ErrorCodeType err, String msg) {
-        log('[onError] err: $err, msg: $msg');
-      },
-      onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
-        log('[onJoinChannelSuccess] connection: ${connection.toJson()} elapsed: $elapsed');
-        setState(() {
-          isJoined = true;
-        });
-      },
-      onLeaveChannel: (RtcConnection connection, RtcStats stats) {
-        log('[onLeaveChannel] connection: ${connection.toJson()} stats: ${stats.toJson()}');
-        setState(() {
-          isJoined = false;
-        });
-      },
-    ));
-
-    await _engine.enableAudio();
-    await _engine.setClientRole(role: ClientRoleType.clientRoleBroadcaster);
-    await _engine.setAudioProfile(
-      profile: AudioProfileType.audioProfileDefault,
-      scenario: AudioScenarioType.audioScenarioGameStreaming,
+    // Register the event handler
+    agoraEngine.registerEventHandler(
+      RtcEngineEventHandler(
+        onTokenPrivilegeWillExpire: (RtcConnection connection, String token) {
+          showMessage('Token expiring');
+          isTokenExpiring = true;
+          setState(() {
+            // fetch a new token when the current token is about to expire
+            fetchToken(uid, channelName, tokenRole);
+          });
+        },
+        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+          showMessage("Local user uid:${connection.localUid} joined the channel");
+          setState(() {
+            _isJoined = true;
+          });
+        },
+        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+          showMessage("Remote user uid:$remoteUid joined the channel");
+          setState(() {
+            _remoteUid = remoteUid;
+          });
+        },
+        onUserOffline: (RtcConnection connection, int remoteUid,
+            UserOfflineReasonType reason) {
+          showMessage("Remote user uid:$remoteUid left the channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
+      ),
     );
   }
 
-  _joinChannel() async {
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      await Permission.microphone.request();
+  void  join() async {
+
+    // Set channel options including the client role and channel profile
+    ChannelMediaOptions options = const ChannelMediaOptions(
+      clientRoleType: ClientRoleType.clientRoleBroadcaster,
+      channelProfile: ChannelProfileType.channelProfileCommunication,
+    );
+
+    channelName = channelTextController.text;
+    if (channelName.isEmpty) {
+      showMessage("Enter a channel name");
+      return;
+    } else {
+      showMessage("Fetching a token ...");
     }
 
     await fetchToken(uid, channelName, tokenRole);
   }
 
-  _leaveChannel() async {
-    await _engine.leaveChannel();
+  void leave() {
     setState(() {
-      isJoined = false;
-      openMicrophone = true;
-      enableSpeakerphone = true;
-      playEffect = false;
-      _enableInEarMonitoring = false;
-      _recordingVolume = 100;
-      _playbackVolume = 100;
-      _inEarMonitoringVolume = 100;
+      _isJoined = false;
+      _remoteUid = null;
     });
-  }
-
-  _switchMicrophone() async {
-    // await await _engine.muteLocalAudioStream(!openMicrophone);
-    await _engine.enableLocalAudio(!openMicrophone);
-    setState(() {
-      openMicrophone = !openMicrophone;
-    });
-  }
-
-  _switchSpeakerphone() async {
-    await _engine.setEnableSpeakerphone(!enableSpeakerphone);
-    setState(() {
-      enableSpeakerphone = !enableSpeakerphone;
-    });
-  }
-
-  _switchEffect() async {
-    if (playEffect) {
-      await _engine.stopEffect(1);
-      setState(() {
-        playEffect = false;
-      });
-    } else {
-      final path = (await _engine.getAssetAbsolutePath("assets/Sound_Horizon.mp3"))!;
-      await _engine.playEffect(soundId: 1, filePath: path, loopCount: 0, pitch: 1, pan: 1, gain: 100, publish: true);
-      // .then((value) {
-      setState(() {
-        playEffect = true;
-      });
-    }
-  }
-
-  _onChangeInEarMonitoringVolume(double value) async {
-    _inEarMonitoringVolume = value;
-    await _engine.setInEarMonitoringVolume(_inEarMonitoringVolume.toInt());
-    setState(() {});
-  }
-
-  _toggleInEarMonitoring(value) async {
-    try {
-      await _engine.enableInEarMonitoring(enabled: value, includeAudioFilters: EarMonitoringFilterType.earMonitoringFilterNone);
-      _enableInEarMonitoring = value;
-      setState(() {});
-    } catch (e) {
-      // Do nothing
-    }
+    agoraEngine.leaveChannel();
   }
 
   @override
-  Widget build(BuildContext context) {
-    final channelProfileType = [
-      ChannelProfileType.channelProfileLiveBroadcasting,
-      ChannelProfileType.channelProfileCommunication,
-    ];
-    final items = channelProfileType
-        .map((e) => DropdownMenuItem(
-              value: e,
-              child: Text(
-                e.toString().split('.')[1],
-              ),
-            ))
-        .toList();
+  void initState() {
+    super.initState();
+    // Set up an instance of Agora engine
+    setupVoiceSDKEngine();
+  }
 
-    return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
+  // Clean up the resources when you leave
+  @override
+  void dispose() async {
+    await agoraEngine.leaveChannel();
+    super.dispose();
+  }
+
+  // Build UI
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      scaffoldMessengerKey: scaffoldMessengerKey,
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Get started with Voice Calling'),
+          ),
+          body: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
             children: [
               TextField(
-                controller: _controller,
-                decoration: const InputDecoration(hintText: 'Channel ID'),
+                controller: channelTextController,
+                decoration: const InputDecoration(hintText: 'Type the channel name here'),
               ),
-              const Text('Channel Profile: '),
-              DropdownButton<ChannelProfileType>(
-                  items: items,
-                  value: _channelProfileType,
-                  onChanged: isJoined
-                      ? null
-                      : (v) async {
-                          setState(() {
-                            _channelProfileType = v!;
-                          });
-                        }),
-              Row(
-                children: [
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: isJoined ? _leaveChannel : _joinChannel,
-                      child: Text('${isJoined ? 'Leave' : 'Join'} channel'),
-                    ),
+
+              // Status text
+              SizedBox(
+                  height: 40,
+                  child:Center(
+                      child:_status()
                   )
+              ),
+              // Button Row
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text("Join"),
+                      onPressed: () => {join()},
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text("Leave"),
+                      onPressed: () => {leave()},
+                    ),
+                  ),
                 ],
               ),
             ],
-          ),
-          Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    ElevatedButton(
-                      onPressed: _switchMicrophone,
-                      child: Text('Microphone ${openMicrophone ? 'on' : 'off'}'),
-                    ),
-                    ElevatedButton(
-                      onPressed: isJoined ? _switchSpeakerphone : null,
-                      child: Text(enableSpeakerphone ? 'Speakerphone' : 'Earpiece'),
-                    ),
-                    if (!kIsWeb)
-                      ElevatedButton(
-                        onPressed: isJoined ? _switchEffect : null,
-                        child: Text('${playEffect ? 'Stop' : 'Play'} effect'),
-                      ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const Text('RecordingVolume:'),
-                        Slider(
-                          value: _recordingVolume,
-                          min: 0,
-                          max: 400,
-                          divisions: 5,
-                          label: 'RecordingVolume',
-                          onChanged: isJoined
-                              ? (double value) async {
-                                  setState(() {
-                                    _recordingVolume = value;
-                                  });
-                                  await _engine.adjustRecordingSignalVolume(value.toInt());
-                                }
-                              : null,
-                        )
-                      ],
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        const Text('PlaybackVolume:'),
-                        Slider(
-                          value: _playbackVolume,
-                          min: 0,
-                          max: 400,
-                          divisions: 5,
-                          label: 'PlaybackVolume',
-                          onChanged: isJoined
-                              ? (double value) async {
-                                  setState(() {
-                                    _playbackVolume = value;
-                                  });
-                                  await _engine.adjustPlaybackSignalVolume(value.toInt());
-                                }
-                              : null,
-                        )
-                      ],
-                    ),
-                    Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Row(mainAxisSize: MainAxisSize.min, children: [
-                          const Text('InEar Monitoring Volume:'),
-                          Switch(
-                            value: _enableInEarMonitoring,
-                            onChanged: isJoined ? _toggleInEarMonitoring : null,
-                            activeTrackColor: Colors.grey[350],
-                            activeColor: Colors.white,
-                          )
-                        ]),
-                        if (_enableInEarMonitoring)
-                          SizedBox(
-                              width: 300,
-                              child: Slider(
-                                value: _inEarMonitoringVolume,
-                                min: 0,
-                                max: 100,
-                                divisions: 5,
-                                label: 'InEar Monitoring Volume $_inEarMonitoringVolume',
-                                onChanged: isJoined ? _onChangeInEarMonitoringVolume : null,
-                              ))
-                      ],
-                    ),
-                  ],
-                ),
-              ))
-        ],
-      ),
+          )),
     );
+  }
+
+  Widget _status(){
+    String statusText;
+
+    if (!_isJoined) {
+      statusText = 'Join a channel';
+    } else if (_remoteUid == null) {
+      statusText = 'Waiting for a remote user to join...';
+    } else {
+      statusText = 'Connected to remote user, uid:$_remoteUid';
+    }
+
+    return Text(
+      statusText,
+    );
+  }
+
+  showMessage(String message) {
+    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
   }
 }
